52829a530b9349518610b906a480707043e1508a
IlyasDeckers <ilyas@phasehosting.io>
2018-02-23 21:12:33 +0100

    chart ou foreach

--- resources/views/rigs.blade.php
+++ resources/views/rigs.blade.php
@@ -3,54 +3,54 @@
 @section('content')
 <div class="row">
     @if(count((array) $rigs) > 0)
-    @foreach($rigs as $key => $value)
-    @if($value !== [])
-    <div class="col-md-12">
-        <div class="card">
-            <!-- <div class="card-header">
-                <h4 class="card-title">{{ $key }}</h4>
-            </div>-->
-            <div class="card-content">
-                <p><b>Rig name:</b> {{ $key }}</p>
-                <p><b>Total hashrate:</b> {{ $value->data['hashrate'] }} MH/s</p>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead class="text-primary">
-                            <tr>
-                                <th>GPU</th>
-                                <th>Hashrate</th>
-                                <th>Temperature</th>
-                                <th>Fanspeed</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($value->data['gpus'] as $k => $v)
-                            <tr>
-                                <td>gpu{{ $k }}</td>
-                                <td>{{ $v['hashrates'] }} MH/s</td>
-                                <td>{{ $v['temperature'] }} &#176;C</td>
-                                <td>{{ $v['fanSpeed'] }} %</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+        @foreach($rigs as $key => $value)
+        @if($value !== [])
+            <div class="col-md-12">
+                <div class="card">
+                    <!-- <div class="card-header">
+                        <h4 class="card-title">{{ $key }}</h4>
+                    </div>-->
+                    <div class="card-content">
+                        <p><b>Rig name:</b> {{ $key }}</p>
+                        <p><b>Total hashrate:</b> {{ $value->data['hashrate'] }} MH/s</p>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead class="text-primary">
+                                    <tr>
+                                        <th>GPU</th>
+                                        <th>Hashrate</th>
+                                        <th>Temperature</th>
+                                        <th>Fanspeed</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($value->data['gpus'] as $k => $v)
+                                    <tr>
+                                        <td>gpu{{ $k }}</td>
+                                        <td>{{ $v['hashrates'] }} MH/s</td>
+                                        <td>{{ $v['temperature'] }} &#176;C</td>
+                                        <td>{{ $v['fanSpeed'] }} %</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">Hashrate</h4>
+        @endif
+        @endforeach
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Hashrate</h4>
+                </div>
+                <div class="card-content">
+                    {!! $chart->render() !!}
+                </div>
+                
             </div>
-            <div class="card-content">
-                {!! $chart->render() !!}
-            </div>
-            
         </div>
-    </div>
-    @endif
-    @endforeach
     @else
     <div class="col-md-12">
         <div class="text-center">
