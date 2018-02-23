d01a757e75ca87df4be9f7d2103f6d001ff6bc1e
IlyasDeckers <ilyas@phasehosting.io>
2018-02-24 00:26:53 +0100

    Frontend updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,38 +84,6 @@
             </div>
         </div>
         <div class="main-panel">
-            <nav class="navbar navbar-transparent navbar-absolute">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <!-- <a class="navbar-brand" href="#"> Profile </a> -->
-                    </div>
-                    <div class="collapse navbar-collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li>
-                                <a href="{{ route('userProfile') }}">
-                                    <i class="material-icons">person</i>
-                                    <p class="hidden-lg hidden-md">Profile</p>
-                                </a>
-                            </li>
-                            <li class="separator hidden-lg hidden-md"></li>
-                            <li>
-                                 <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <i class="material-icons">power_settings_new</i>
-                                    <p class="hidden-lg hidden-md">power_settings_new</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    
-
-                </div>
-            </nav>
             <div class="content" >
                 <div class="container-fluid">
                     <div class="row">
