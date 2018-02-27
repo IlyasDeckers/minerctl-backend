fdfd8ee685151a857d97b6cf9a412368ad2a04b7
IlyasDeckers <ilyas@phasehosting.io>
2018-02-27 10:57:07 +0100

    Use Rigs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
 use App\MinerStatistics;
+use GuzzleHttp\Client;
 use Carbon\Carbon;
 use Ethermine;
 use Claymore;
 use Wallets;
 use Auth;
+use Rigs;
 
 use Redirect;
 use Alert;
@@ -33,30 +34,12 @@ class HomeController extends Controller
   */
   public function index()
   {
-    if (!Auth::user()->wallets->first()) {
-      return view('auth.step2');
-    }
-
     $wallet = Auth::user()->wallets->first();
     $walletAddress = $wallet->address;
 
-    $m = \App\MinerStatistics::where('user_id', auth()->user()->id)
-                ->where('created_at','>=', Carbon::now()->subMinutes('2'))
-                ->get()
-                ->groupBy('rigname');
-
-    $miners = [];
-    foreach($m as $miner) {
-      $miner = $miner->first();
-      $miner->data = json_decode($miner->data);
-      $miners[] = $miner;
-    }
-
-    $ethermine = new Ethermine($walletAddress);
-
     return view('dashboard', [
       'page_title'         => 'Dashboard',
-      'miners'             => $miners,
+      'miners'             => Rigs::getLastStatistics(),
       'notifications_dash' => Auth::user()->notifications->take(10),
       'total_stats'        => Ethermine::getlastStatisticss($walletAddress),
       'wallet'             => Wallets::getWallet($walletAddress)
